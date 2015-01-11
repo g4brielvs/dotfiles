fe70f8a3536e60982dc772dbc8621db89af00fc7
Gabriel Vicente <gabrielusvicente@gmail.com>
2015-01-11 09:00:42 -0200

    Add zsh

---
+++ git/aliases.zsh
@@ -0,0 +1,8 @@
+
+alias gc='git commit'
+alias gd='git diff'
+alias gs='git status -sb' 
+alias gp='git push origin HEAD'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
---
+++ zsh/config.zsh
@@ -0,0 +1,28 @@
+export LSCOLORS="exfxcxdxbxegedabagacad"
+export CLICOLOR=true
+
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
+setopt APPEND_HISTORY 
+setopt HIST_REDUCE_BLANKS
+setopt EXTENDED_HISTORY 
+setopt PROMPT_SUBST
+setopt CORRECT
+setopt COMPLETE_IN_WORD
+setopt IGNORE_EOF
+
+# don't expand aliases _before_ completion has finished
+#   like: git comm-[tab]
+setopt complete_aliases
+
+zle -N newtab
+
+bindkey '^[^[[D' backward-word
+bindkey '^[^[[C' forward-word
+bindkey '^[[5D' beginning-of-line
+bindkey '^[[5C' end-of-line
+bindkey '^[[3~' delete-char
+bindkey '^[^N' newtab
+bindkey '^?' backward-delete-char
